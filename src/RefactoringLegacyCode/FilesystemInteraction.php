88946b18fc48ae5b35fddb27f36026db292885f4
Andi Rückauer <arueckauer@gmail.com>
2021-09-24 15:16:22 +0200

    Add filesystem interaction example

---
+++ src/RefactoringLegacyCode/FilesystemInteraction.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RefactoringLegacyCode;
+
+use function file_get_contents;
+use function file_put_contents;
+
+class FilesystemInteraction
+{
+// phpcs:disable
+//    Code before refactoring (no DI):
+//    public function countHit(): void
+//    {
+//        $count = file_get_contents(__DIR__ . '/hit_counter.txt');
+//        file_put_contents(__DIR__ . '/hit_counter.txt', ++$count);
+//    }
+// phpcs:enable
+
+    public function countHit(string $file = __DIR__ . '/hit_counter.txt'): void
+    {
+        $count = file_get_contents($file);
+        file_put_contents($file, ++$count);
+    }
+}
---
+++ test/RefactoringLegacyCode/FilesystemInteractionTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RefactoringLegacyCodeTest;
+
+use org\bovigo\vfs\vfsStream;
+use PHPUnit\Framework\TestCase;
+use RefactoringLegacyCode\FilesystemInteraction;
+
+class FilesystemInteractionTest extends TestCase
+{
+    /**
+     * @covers \RefactoringLegacyCode\FilesystemInteraction::countHit
+     */
+    public function test_countHit(): void
+    {
+        $vfs = vfsStream::setup();
+
+        $hitCounterFile = vfsStream::newFile('test_hit_counter.txt')
+            ->at($vfs)
+            ->setContent('1337');
+
+        $legacyCode = new FilesystemInteraction();
+        $legacyCode->countHit($hitCounterFile->url());
+
+        self::assertSame(
+            '1338',
+            $hitCounterFile->getContent()
+        );
+    }
+}
